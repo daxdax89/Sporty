fa843edf8ffd137a1f2b64213721ec56ea35cc8a
DaX <daxdax89@msn.com>
2019-12-26 15:40:32 +0100

    changes

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,74 +1,91 @@
 import * as WebBrowser from "expo-web-browser";
 import React from "react";
 import {
-  Image,
   Platform,
   ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
-  View
+  View,
+  ImageBackground,
+  TextInput
 } from "react-native";
+import { Icon } from "react-native-elements";
 
-import { MonoText } from "../components/StyledText";
+// import { MonoText } from "../components/StyledText";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}
-      >
-        {/* <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require("../assets/images/robot-dev.png")
-                : require("../assets/images/robot-prod.png")
-            }
-            style={styles.welcomeImage}
-          />
-        </View> */}
-
-        <View style={styles.getStartedContainer}>
-          {/* <DevelopmentModeNotice /> */}
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-          >
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}
-        >
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
+    <ImageBackground
+      source={require("../assets/images/soccer-field.jpg")}
+      style={styles.background}
+    >
+      <View style={styles.iconHolder}>
+        <Icon
+          name="sign-in"
+          type="font-awesome"
+          color="white"
+          size={100}
+        ></Icon>
       </View>
-    </View>
+
+      <TextInput inlineImageLeft="search_icon" style={styles.input} />
+    </ImageBackground>
+    // <View style={styles.container}>
+    //   <ScrollView
+    //     style={styles.container}
+    //     contentContainerStyle={styles.contentContainer}
+    //   >
+    //     {/* <View style={styles.welcomeContainer}>
+    //       <Image
+    //         source={
+    //           __DEV__
+    //             ? require("../assets/images/robot-dev.png")
+    //             : require("../assets/images/robot-prod.png")
+    //         }
+    //         style={styles.welcomeImage}
+    //       />
+    //     </View> */}
+
+    //     <View style={styles.getStartedContainer}>
+    //       {/* <DevelopmentModeNotice /> */}
+
+    //       <Text style={styles.getStartedText}>Get started by openings</Text>
+
+    //       <View
+    //         style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
+    //       >
+    //         <MonoText>screens/HomeScreen.js</MonoText>
+    //       </View>
+
+    //       <Text style={styles.getStartedText}>
+    //         Change this text and your app will automatically reload.
+    //       </Text>
+    //     </View>
+
+    //     <View style={styles.helpContainer}>
+    //       <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
+    //         <Text style={styles.helpLinkText}>
+    //           Help, it didn’t automatically reload!
+    //         </Text>
+    //       </TouchableOpacity>
+    //     </View>
+    //   </ScrollView>
+
+    //   <View style={styles.tabBarInfoContainer}>
+    //     <Text style={styles.tabBarInfoText}>
+    //       This is a tab bar. You can edit it in:
+    //     </Text>
+
+    //     <View
+    //       style={[styles.codeHighlightContainer, styles.navigationFilename]}
+    //     >
+    //       <MonoText style={styles.codeHighlightText}>
+    //         navigation/MainTabNavigator.js
+    //       </MonoText>
+    //     </View>
+    //   </View>
+    // </View>
   );
 }
 
@@ -112,90 +129,102 @@ function handleHelpPress() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
+    width: "100%",
+    height: "100%",
     flex: 1,
-    backgroundColor: "#fff"
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: "rgba(0,0,0,0.4)",
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: "center"
-  },
-  contentContainer: {
-    paddingTop: 30
-  },
-  welcomeContainer: {
-    alignItems: "center",
-    marginTop: 10,
-    marginBottom: 20
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: "contain",
-    marginTop: 3,
-    marginLeft: -10
-  },
-  getStartedContainer: {
-    alignItems: "center",
-    marginHorizontal: 50
-  },
-  homeScreenFilename: {
-    marginVertical: 7
-  },
-  codeHighlightText: {
-    color: "rgba(96,100,109, 0.8)"
-  },
-  codeHighlightContainer: {
-    backgroundColor: "rgba(0,0,0,0.05)",
-    borderRadius: 3,
-    paddingHorizontal: 4
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    lineHeight: 24,
-    textAlign: "center"
-  },
-  tabBarInfoContainer: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3
-      },
-      android: {
-        elevation: 20
-      }
-    }),
-    alignItems: "center",
-    backgroundColor: "#fbfbfb",
-    paddingVertical: 20
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    textAlign: "center"
-  },
-  navigationFilename: {
-    marginTop: 5
-  },
-  helpContainer: {
-    marginTop: 15,
     alignItems: "center"
   },
-  helpLink: {
-    paddingVertical: 15
+  input: {
+    color: "black"
   },
-  helpLinkText: {
-    fontSize: 14,
-    color: "#2e78b7"
+  iconHolder: {
+    marginTop: 200
   }
+  //   container: {
+  //     flex: 1,
+  //     backgroundColor: "#fff"
+  //   },
+  //   developmentModeText: {
+  //     marginBottom: 20,
+  //     color: "rgba(0,0,0,0.4)",
+  //     fontSize: 14,
+  //     lineHeight: 19,
+  //     textAlign: "center"
+  //   },
+  //   contentContainer: {
+  //     paddingTop: 30
+  //   },
+  //   welcomeContainer: {
+  //     alignItems: "center",
+  //     marginTop: 10,
+  //     marginBottom: 20
+  //   },
+  //   //   welcomeImage: {
+  //   //     width: 100,
+  //   //     height: 80,
+  //   //     resizeMode: "contain",
+  //   //     marginTop: 3,
+  //   //     marginLeft: -10
+  //   //   },
+  //   getStartedContainer: {
+  //     alignItems: "center",
+  //     marginHorizontal: 50
+  //   },
+  //   homeScreenFilename: {
+  //     marginVertical: 7
+  //   },
+  //   codeHighlightText: {
+  //     color: "rgba(96,100,109, 0.8)"
+  //   },
+  //   codeHighlightContainer: {
+  //     backgroundColor: "rgba(0,0,0,0.05)",
+  //     borderRadius: 3,
+  //     paddingHorizontal: 4
+  //   },
+  //   getStartedText: {
+  //     fontSize: 17,
+  //     color: "rgba(96,100,109, 1)",
+  //     lineHeight: 24,
+  //     textAlign: "center"
+  //   },
+  //   tabBarInfoContainer: {
+  //     position: "absolute",
+  //     bottom: 0,
+  //     left: 0,
+  //     right: 0,
+  //     ...Platform.select({
+  //       ios: {
+  //         shadowColor: "black",
+  //         shadowOffset: { width: 0, height: -3 },
+  //         shadowOpacity: 0.1,
+  //         shadowRadius: 3
+  //       },
+  //       android: {
+  //         elevation: 20
+  //       }
+  //     }),
+  //     alignItems: "center",
+  //     backgroundColor: "#fbfbfb",
+  //     paddingVertical: 20
+  //   },
+  //   tabBarInfoText: {
+  //     fontSize: 17,
+  //     color: "rgba(96,100,109, 1)",
+  //     textAlign: "center"
+  //   },
+  //   navigationFilename: {
+  //     marginTop: 5
+  //   },
+  //   helpContainer: {
+  //     marginTop: 15,
+  //     alignItems: "center"
+  //   },
+  //   helpLink: {
+  //     paddingVertical: 15
+  //   },
+  //   helpLinkText: {
+  //     fontSize: 14,
+  //     color: "#2e78b7"
+  //   }
 });
